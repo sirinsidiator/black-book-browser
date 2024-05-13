e78225a8314354e1c8c8b87c88e97b602293de94
sirinsidiator <insidiator@cmos.at>
2024-05-13 17:39:23 +0200

    added copy button to detail entries

--- src/lib/content/DetailEntry.svelte
+++ src/lib/content/DetailEntry.svelte
@@ -1,12 +1,45 @@
 <script lang="ts">
+    import { toastController } from 'ionic-svelte';
+    import { copyOutline } from 'ionicons/icons';
+
     export let icon: string;
     export let label: string;
+
+    let value: HTMLIonLabelElement;
+
+    async function copy() {
+        try {
+            const text = value.innerText;
+            await navigator.clipboard.writeText(text);
+            const toast = await toastController.create({
+                message: 'Copied to clipboard',
+                duration: 2000,
+                color: 'success',
+                position: 'bottom'
+            });
+            await toast.present();
+        } catch (err) {
+            console.warn('Failed to copy', err);
+
+            const toast = await toastController.create({
+                message: 'Could not copy to clipboard',
+                duration: 2000,
+                color: 'danger',
+                position: 'bottom'
+            });
+            await toast.present();
+        }
+    }
 </script>
 
 <ion-item>
     <ion-icon {icon} />
     <ion-label class="label">{label}</ion-label>
-    <ion-label><slot /></ion-label>
+    <ion-label bind:this={value}><slot /></ion-label>
+    <!-- eslint-disable-next-line svelte/valid-compile -->
+    <ion-button fill="clear" color="medium" slot="end" size="small" on:click={copy}>
+        <ion-icon slot="icon-only" icon={copyOutline} />
+    </ion-button>
 </ion-item>
 
 <style>
